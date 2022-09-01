9037d96d90f466044d20d95a024acc57eed13ab4
yulizi <yulizi.zero@bim-group.com>
2022-09-01 17:30:15 +0800

    [router] 路由模組化

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
-const { Pool, Client } = require("pg");
 const Inert = require("@hapi/inert");
 const Vision = require("@hapi/vision");
 const HapiSwagger = require("hapi-swagger");
-const Pack = require("./package");
-const { DateTime } = require("luxon");
 const Joi = require("joi");
+const { DateTime } = require("luxon");
+
+const Pack = require("./package");
 require("dotenv").config();
 
 const init = async () => {
-  const client = new Pool({
-    connectionString: process.env.POSTGRES_CONN,
-  });
-
   const server = Hapi.server({
     port: process.env.PORT,
     host: "0.0.0.0",
@@ -24,26 +20,6 @@ const init = async () => {
     routes: {
       cors: true,
     },
-  });
-
-  server.ext("onPreResponse", function (request, reply) {
-    const response = request.response;
-    if (response && response.header && typeof response.header === "function") {
-      response.header("Access-Control-Allow-Headers", "*");
-      response.header("Access-Control-Allow-Origin", "*");
-      response.header("Access-Control-Allow-Methods", "*");
-      // response.header("Access-Control-Allow-Credentials", true);
-    }
-
-    try {
-      if (request.method === "options") {
-        return request.response.code(200);
-      } else {
-        return request.response;
-      }
-    } catch (err) {
-      console.log(err);
-    }
   });
 
   await server.register({
@@ -82,64 +58,7 @@ const init = async () => {
     },
   });
 
-  server.route({
-    method: "GET",
-    path: "/inspection/{id}/carGpsTrack",
-    options: {
-      tags: ["api"],
-      cors: { origin: ["*"], credentials: true },
-      validate: {
-        params: Joi.object({
-          id: Joi.number().required(),
-          // .description('the id for the todo item'),
-        }),
-      },
-    },
-    handler: async (request, h) => {
-      const inspectionId = request.params.id;
-      const result = await client.query(
-        'SELECT * FROM "carGpsTrack" WHERE inspectionId = $1 ORDER BY createdAt DESC',
-        [inspectionId]
-      );
-      return result.rows || {};
-    },
-  });
-
-  server.route({
-    method: "POST",
-    path: "/inspection/{id}/carGpsTrack",
-    options: {
-      tags: ["api"],
-      cors: { origin: ["*"], credentials: true },
-      validate: {
-        params: Joi.object({
-          id: Joi.number().required(),
-          // .description('the id for the todo item'),
-        }),
-        payload: Joi.object({
-          createdAt: Joi.date(),
-          lat: Joi.number().default("0"),
-          long: Joi.number().default("0"),
-        }),
-      },
-    },
-    handler: async (request, h) => {
-      const inspectionId = request.params.id;
-      const { createdAt, lat, long } = request.payload;
-      // const createdAtJSDate = DateTime.fromJSDate(createdAt).toJSDate();
-      const result = await client.query(
-        'INSERT INTO "carGpsTrack" (createdAt, lat, long, inspectionId) VALUES($1, $2, $3, $4)',
-        [createdAt, lat, long, inspectionId]
-      );
-
-      return {
-        code: 200,
-        result: {
-          id: result.rows[0],
-        },
-      };
-    },
-  });
+  await server.register(require("./router/carGpsTrack"));
 
   await server.start();
 
---
+++ router/carGpsTrack.js
@@ -0,0 +1,72 @@
+const Joi = require("joi");
+const { Pool } = require("pg");
+
+exports.plugin = {
+  //pkg: require('./package.json'),
+  name: "carGpsTrack",
+  version: "1.0.0",
+  register: async function (server, options) {
+    const client = new Pool({
+      connectionString: process.env.POSTGRES_CONN,
+    });
+
+    server.route({
+      method: "GET",
+      path: "/inspection/{id}/carGpsTrack",
+      options: {
+        tags: ["api"],
+        cors: { origin: ["*"], credentials: true },
+        validate: {
+          params: Joi.object({
+            id: Joi.number().required(),
+            // .description('the id for the todo item'),
+          }),
+        },
+      },
+      handler: async (request, h) => {
+        const inspectionId = request.params.id;
+        const result = await client.query(
+          'SELECT * FROM "carGpsTrack" WHERE inspectionId = $1 ORDER BY createdAt DESC',
+          [inspectionId]
+        );
+        return result.rows || {};
+      },
+    });
+
+    server.route({
+      method: "POST",
+      path: "/inspection/{id}/carGpsTrack",
+      options: {
+        tags: ["api"],
+        cors: { origin: ["*"], credentials: true },
+        validate: {
+          params: Joi.object({
+            id: Joi.number().required(),
+            // .description('the id for the todo item'),
+          }),
+          payload: Joi.object({
+            createdAt: Joi.date(),
+            lat: Joi.number().default("0"),
+            long: Joi.number().default("0"),
+          }),
+        },
+      },
+      handler: async (request, h) => {
+        const inspectionId = request.params.id;
+        const { createdAt, lat, long } = request.payload;
+        // const createdAtJSDate = DateTime.fromJSDate(createdAt).toJSDate();
+        const result = await client.query(
+          'INSERT INTO "carGpsTrack" (createdAt, lat, long, inspectionId) VALUES($1, $2, $3, $4)',
+          [createdAt, lat, long, inspectionId]
+        );
+
+        return {
+          code: 200,
+          result: {
+            id: result.rows[0],
+          },
+        };
+      },
+    });
+  },
+};
